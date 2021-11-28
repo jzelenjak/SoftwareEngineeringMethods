30a01e237873f168375c48e8376a8c5f0cfc7e20
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2021-11-28 23:31:25 +0100

    Change the PasswordConfig.java to make the encoding strength a valid number

--- microservices/authentication/src/main/java/nl/tudelft/sem/authentication/security/PasswordConfig.java
+++ microservices/authentication/src/main/java/nl/tudelft/sem/authentication/security/PasswordConfig.java
@@ -9,6 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PasswordConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(42);
+        return new BCryptPasswordEncoder(27);
     }
 }
