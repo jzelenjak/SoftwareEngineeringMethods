bb33ac70bdfd1028c1ebe02b1aeb007bd3d6ae9d
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2021-11-28 23:25:27 +0100

    Add @Configuration annotation that fixed a bean not found issue in the AuthenticationSecurityConfig.java

--- microservices/authentication/src/main/java/nl/tudelft/sem/authentication/jwt/JwtSecretKey.java
+++ microservices/authentication/src/main/java/nl/tudelft/sem/authentication/jwt/JwtSecretKey.java
@@ -4,9 +4,12 @@ import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class JwtSecretKey {
     private final JwtConfig jwtConfig;
+
     @Autowired
     public JwtSecretKey(JwtConfig jwtConfig) {
         this.jwtConfig = jwtConfig;
