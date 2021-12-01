4e1203097963804cf2ee12b8214e1270b0f4b5d3
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2021-12-01 15:15:39 +0100

    Delete UserAlreadyExistsException.java file ...
    Change password encoding strength from 27 to 10

--- microservices/authentication/src/main/java/nl/tudelft/sem/authentication/exceptions/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package nl.tudelft.sem.authentication.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Username already taken")
-public class UserAlreadyExistsException extends Exception {
-    private static final long serialVersionUID = 87654546255446278L;
-}
