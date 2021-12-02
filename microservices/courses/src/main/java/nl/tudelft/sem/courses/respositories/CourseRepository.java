83962a4bb9e1f8d56310b9df5afb72f882f3fa1b
Taneshwar Pranav Parankusam <t.p.parankusam@student.tudelft.nl>
2021-12-02 15:25:51 +0100

    Added field for start date to Courses.
    Ensured that the entities now work properly. Jpa Repositories are properly initialized with this.

--- microservices/courses/src/main/java/nl/tudelft/sem/courses/respositories/CourseRepository.java
+++ microservices/courses/src/main/java/nl/tudelft/sem/courses/respositories/CourseRepository.java
@@ -2,6 +2,9 @@ package nl.tudelft.sem.courses.respositories;
 
 import nl.tudelft.sem.courses.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CourseRepository extends JpaRepository<Course, Long> {
+@Repository
+public interface CourseRepository extends CrudRepository<Course, Long> {
 }
--- microservices/courses/src/test/java/nl/tudelft/sem/courses/GradeTest.java
+++ microservices/courses/src/test/java/nl/tudelft/sem/courses/GradeTest.java
@@ -14,8 +14,9 @@ public class GradeTest {
     @Test
     public void constructorTest(){
         Course course = new Course("CSE2215");
-        User user = new User(1, "bob", "mark", Role.STUDENT);
-        Grade grade = new Grade(course, user, 2.0f);
+        User user = new User(1,  Role.STUDENT);
+        long id = 1;
+        Grade grade = new Grade(id, course, user, 2.0f);
 
         assertNotNull(grade);
     }
@@ -23,9 +24,11 @@ public class GradeTest {
     @Test
     public void testingEqualsMethod(){
         Course course = new Course("CSE2215");
-        User user = new User(1, "bob", "mark", Role.STUDENT);
-        Grade grade = new Grade(course, user, 2.0f);
-        Grade grade2 = new Grade(course, user, 3.0f);
+        User user = new User(1, Role.STUDENT);
+        long id = 1;
+        long id2 = 2;
+        Grade grade = new Grade(id, course, user, 2.0f);
+        Grade grade2 = new Grade(id, course, user, 3.0f);
 
         assertNotEquals(grade, grade2);
     }
