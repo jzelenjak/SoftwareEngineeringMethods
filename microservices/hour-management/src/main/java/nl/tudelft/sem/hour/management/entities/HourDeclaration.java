eb0e1c6ff8190adbd962d604abed872a6eb27313
Mehmet Alp Sozuduz <M.A.Sozuduz@student.tudelft.nl>
2021-12-02 10:12:26 +0100

    I hate checkstyle

--- microservices/hour-management/src/main/java/nl/tudelft/sem/hour/management/entities/HourDeclaration.java
+++ microservices/hour-management/src/main/java/nl/tudelft/sem/hour/management/entities/HourDeclaration.java
@@ -2,7 +2,11 @@ package nl.tudelft.sem.hour.management.entities;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
