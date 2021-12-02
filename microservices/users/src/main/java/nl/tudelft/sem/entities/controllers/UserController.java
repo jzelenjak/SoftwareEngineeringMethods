441c82bccdd80256e0d64e3f105ccc1c683f8779
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2021-12-02 08:34:23 +0100

    Minor modifications 2: * Add UserService.java file * Add some template methods to UserService.java and UserController.java * Fix dependency issue in build.gradle

--- microservices/users/src/main/java/nl/tudelft/sem/entities/controllers/UserController.java
+++ microservices/users/src/main/java/nl/tudelft/sem/entities/controllers/UserController.java
@@ -1,17 +1,30 @@
 package nl.tudelft.sem.entities.controllers;
 
+import nl.tudelft.sem.entities.services.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Controller class for users.
  */
 @RestController
-@RequestMapping
+@RequestMapping("/api/users")
 public class UserController {
-    public UserController() {
+    private final transient UserService userService;
 
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void registerUser(HttpServletRequest req, HttpServletResponse res) {
+        // register user
     }
 
     @GetMapping
---
+++ microservices/users/src/main/java/nl/tudelft/sem/entities/services/UserService.java
@@ -0,0 +1,24 @@
+package nl.tudelft.sem.entities.services;
+
+import nl.tudelft.sem.entities.entities.User;
+import nl.tudelft.sem.entities.entities.UserRole;
+import nl.tudelft.sem.entities.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final transient UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean registerUser(String netid, String name, String surname, UserRole role) {
+        if (this.userRepository.findByUsername(netid).isPresent()) {
+            return false;
+        }
+        this.userRepository.save(new User(netid, null, name, surname, role));
+        return true;
+    }
+}
