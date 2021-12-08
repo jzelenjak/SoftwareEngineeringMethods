a2b3d25b99b62de9ef0fcb0da39a3b331e54e9ac
Mihnea Toader <m.a.toader-1@student.tudelft.nl>
2021-12-08 23:19:26 +0100

    Make GatewayConfig a Configuration

--- microservices/hiring-procedure/src/main/java/nl/tudelft/sem/hiring/procedure/utils/GatewayConfig.java
+++ microservices/hiring-procedure/src/main/java/nl/tudelft/sem/hiring/procedure/utils/GatewayConfig.java
@@ -2,17 +2,14 @@ package nl.tudelft.sem.hiring.procedure.utils;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
-@Component
+@Configuration
+@ConfigurationProperties(prefix = "gateway")
 public class GatewayConfig {
     private String host;
     private int port;
-
-    public GatewayConfig() {
-        this.host = "localhost";
-        this.port = 8080;
-    }
 }
