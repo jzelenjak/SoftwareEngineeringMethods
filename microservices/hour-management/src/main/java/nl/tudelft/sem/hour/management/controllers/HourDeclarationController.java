018f5152dafcd64ab11763b9ee510a61c399e1b6
Mehmet Alp Sozuduz <M.A.Sozuduz@student.tudelft.nl>
2021-12-21 21:45:55 +0300

    Modify controller and StatisticsService for new endpoint
    Modify these classes to include support for retrieving hours for several TAs for several courses

--- microservices/hour-management/src/main/java/nl/tudelft/sem/hour/management/controllers/HourDeclarationController.java
+++ microservices/hour-management/src/main/java/nl/tudelft/sem/hour/management/controllers/HourDeclarationController.java
@@ -1,14 +1,20 @@
 package nl.tudelft.sem.hour.management.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.Data;
 import nl.tudelft.sem.hour.management.config.GatewayConfig;
 import nl.tudelft.sem.hour.management.dto.HourDeclarationRequest;
 import nl.tudelft.sem.hour.management.dto.StatisticsRequest;
+import nl.tudelft.sem.hour.management.dto.StudentHoursTuple;
+import nl.tudelft.sem.hour.management.dto.UserHoursStatisticsRequest;
 import nl.tudelft.sem.hour.management.entities.HourDeclaration;
 import nl.tudelft.sem.hour.management.repositories.HourDeclarationRepository;
 import nl.tudelft.sem.hour.management.services.NotificationService;
@@ -325,4 +331,50 @@ public class HourDeclarationController {
             return Mono.just(result.toString());
         });
     }
+
+    /**
+     * Retrieves the total amount of hours declared by listed students for listed courses.
+     *
+     * @param headers                    headers of the request.
+     * @param userHoursStatisticsRequest requests containing student ids, course ids,
+     *                                   minimum hours and amount of results.
+     * @return total amount of hours declared by students for specified courses.
+     */
+    @PostMapping("/statistics/total-user-hours")
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody
+    Mono<String> getTotalHoursPerStudentPerCourse(@RequestHeader HttpHeaders headers,
+                                                  @RequestBody UserHoursStatisticsRequest
+                                                          userHoursStatisticsRequest) {
+        AsyncValidator head = AsyncValidator.Builder.newBuilder()
+                .addValidators(
+                        new AsyncAuthValidator(gatewayConfig, jwtUtils),
+                        new AsyncRoleValidator(gatewayConfig, jwtUtils,
+                                Set.of(Roles.ADMIN, Roles.LECTURER))
+                ).build();
+
+        return head.validate(headers, "").flatMap(valid -> {
+            // Convert statistics to json object
+            JsonObject jsonObject = new JsonObject();
+
+            // Fetch the declaration statistics
+            statisticsService.getTotalHoursPerStudentPerCourse(
+                            userHoursStatisticsRequest.getStudentIds(),
+                            userHoursStatisticsRequest.getCourseIds(),
+                            userHoursStatisticsRequest.getMinHours(),
+                            userHoursStatisticsRequest.getAmount())
+                    .forEach(t -> jsonObject.addProperty(t.getStudentId().toString(),
+                            t.getTotalHours()));
+
+            // Check if at least one of the students declared hours for any of the courses
+            if (jsonObject.size() == 0) {
+                return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "No statistics found for the specified course and students."));
+            }
+
+            // Return the response object
+            return Mono.just(jsonObject.toString());
+        });
+    }
+
 }
