b3ad1fcd3af960ebfe45b2a77e51ac47abed5510
Taneshwar Pranav Parankusam <t.p.parankusam@student.tudelft.nl>
2022-01-09 13:19:16 +0100

    This might fix the issue? Not sure got to check git lab for this one.

--- microservices/courses/src/main/java/nl/tudelft/sem/courses/services/CourseService.java
+++ microservices/courses/src/main/java/nl/tudelft/sem/courses/services/CourseService.java
@@ -216,7 +216,7 @@ public class CourseService {
         for (Long userId : recommendationRequest.getUserIds()) {
             Grade grade = getGrade(userId, recommendationRequest.getCourseId());
             if (grade != null && grade.getGradeValue() >= recommendationRequest.getMinGrade()) {
-                Map.Entry<Long, Float> entry =
+                AbstractMap.SimpleEntry<Long, Float> entry =
                         new AbstractMap.SimpleEntry(userId, grade.getGradeValue());
                 list.add(entry);
             }
